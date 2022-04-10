c4d6ffd073c1bee35f6403d084040d51456d48ee
Luigi Pinna» <luigipinna3@gmail.com>
2022-04-10 18:24:08 +0200

    created deploy code and ether node

--- ethereum/hardhat.config.js
+++ ethereum/hardhat.config.js
@@ -1,1 +1,14 @@
-"###"
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.1",
+  defaultNetwork: "ropsten",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: process.env.DEV_API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+};
